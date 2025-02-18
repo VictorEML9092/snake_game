bbffc33ca124140b26bc2b0fbe03ca315dc094dc
Victor Eduardo Mendoza-Lopez <victormendoza2008@outlook.com>
2025-02-18 17:01:09 -0600

    Full Game

--- lib/main.dart
+++ lib/main.dart
@@ -161,4 +161,106 @@ class HomePageState extends State<HomePage> {
           );
         });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Fondo negro de la pantalla principal.
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (direction != 'up' && details.delta.dy > 0) {
+                  direction =
+                      'down'; // Cambia la dirección hacia abajo si no estaba yendo hacia arriba.
+                } else if (direction != 'down' && details.delta.dy < 0) {
+                  direction =
+                      'up'; // Cambia la dirección hacia arriba si no estaba yendo hacia abajo.
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (direction != 'left' && details.delta.dx > 0) {
+                  direction =
+                      'right'; // Cambia la dirección hacia la derecha si no estaba yendo hacia la izquierda.
+                } else if (direction != 'right' && details.delta.dx < 0) {
+                  direction =
+                      'left'; // Cambia la dirección hacia la izquierda si no estaba yendo hacia la derecha.
+                }
+              },
+              child: GridView.builder(
+                  physics:
+                      NeverScrollableScrollPhysics(), // Evita que la cuadrícula sea desplazable.
+                  itemCount:
+                      numberOfSquares, // Número total de cuadros en la cuadrícula.
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount:
+                          20), // Define la cantidad de columnas en la cuadrícula.
+                  itemBuilder: (BuildContext context, int index) {
+                    if (snakePosition.contains(index)) {
+                      // Si la posición actual es parte de la serpiente.
+                      return Center(
+                        child: Container(
+                          padding: EdgeInsets.all(2),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Container(
+                              color: Colors
+                                  .white, // Dibuja la serpiente en color blanco.
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                    if (index == food) {
+                      // Si la posición actual es la comida.
+                      return Container(
+                        padding: EdgeInsets.all(2),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Container(
+                              color: Colors
+                                  .green, // Dibuja la comida en color verde.
+                            )),
+                      );
+                    } else {
+                      // Cuadrícula vacía.
+                      return Container(
+                        padding: EdgeInsets.all(2),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: Container(
+                              color: Colors.grey[
+                                  900], // Dibuja el fondo de la cuadrícula.
+                            )),
+                      );
+                    }
+                  }),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap:
+                      startGame, // Inicia el juego cuando se toca el texto "start".
+                  child: Text(
+                    's t a r t',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                Text(
+                  '@ c r e a t e d b y k o k o',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
